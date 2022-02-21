db06830b621b32f73aa4f5b71217a8bc16ffc94e
SigmaDemon <sebastianpoiana2015@gmail.com>
2022-02-21 12:13:25 +0000

    Uploaded the first files

---
+++ javascript-full-beginners-learning/comments.js
@@ -0,0 +1,7 @@
+// Comments - these are lines of code that get ignored by the browser
+var number = 5; // this is an in-line comment
+
+/*
+This is a multi-line comment
+We can put as much text as we want
+*/
---
+++ javascript-full-beginners-learning/data-types-and-variables.js
@@ -0,0 +1,18 @@
+// Data Types - those are undefined, null, boolean, string, symbol and object
+// Variables - these allow computers to store data 
+
+var myName = "Beau";
+
+mYName = 8;
+
+let ourName = 'The Boys!'
+
+const pi = 3.14;
+
+/* The difference between var, let and const:
+
+   -Var is going to be used throughout the whole program
+   -Let will only be used within the scope of where we declared that program.
+   -Const is a variable that should NEVER change.
+
+*/
